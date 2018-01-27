3b2fcfb24a1bb2b555a846278c8affeee6ae6983
Paul Kelly <pkelly429@gmail.com>
2018-01-27 19:17:35 +0000

    Working on jumping.

--- GJJ_Why_Dog_Why_3D/Assets/Scripts/PlayerController.cs
+++ GJJ_Why_Dog_Why_3D/Assets/Scripts/PlayerController.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public int PlayerNumber;
-    public Transform GroundedRaycast;
 
-    public const float MaxHorizontalVelocity = 30f;
-    public const float HorizontalAcceleration = 500f;
+    public Touching Grounder;
+    public Touching WallHugger;
+
+    public const float MaxHorizontalVelocity = 20f;
+    public const float HorizontalAcceleration = 50f;
+    public const float GroundRaycastLength = 0.1f;
+
+    public const float JumpVelocity = 20;
 
     private Player _player;
     private Rigidbody _rigidbody;
 
-    private Vector2 moveAxis;
+    private Vector2 _moveAxis;
     private float _horizontalVelocity;
+
+    public bool CanJump { get { return !_jumping && (_isGrounded || _isTouchingWall); } }
+
+    private bool _jumpDown;
     private bool _isGrounded;
+    private bool _isTouchingWall;
+    private bool _jumping;
 
     private void Start()
     {
@@ -24,19 +35,36 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        _player.GetButtonDown("Jump");
+        
+        if(CanJump && _player.GetButtonDown("Jump"))
+        {
+            _jumping = true;
+            _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, JumpVelocity, _rigidbody.velocity.z);
+        }
 
-        moveAxis = _player.GetAxis2D("MoveX", "MoveY");
+        if(_jumping && _player.GetButtonUp("Jump"))
+        {
+            _jumping = false;
+            _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, Mathf.Min(_rigidbody.velocity.y, 0), _rigidbody.velocity.z);
+        }
+
+        _moveAxis = _player.GetAxis2D("MoveX", "MoveY");
 
     }
 
+    private Ray _groundRaycast;
+    private RaycastHit[] _results;
     private void FixedUpdate()
     {
+        _isGrounded = Grounder.IsTouching;
+        _isTouchingWall = WallHugger.IsTouching;
+
         _horizontalVelocity = _rigidbody.velocity.x;
 
-        _horizontalVelocity += moveAxis.x * HorizontalAcceleration * Time.fixedDeltaTime;
+        _horizontalVelocity += _moveAxis.x * HorizontalAcceleration * Time.fixedDeltaTime;
         _horizontalVelocity = Mathf.Clamp(_horizontalVelocity, -MaxHorizontalVelocity, MaxHorizontalVelocity);
 
         _rigidbody.velocity = new Vector3(_horizontalVelocity, _rigidbody.velocity.y, 0);
+
     }
 }
---
+++ GJJ_Why_Dog_Why_3D/Assets/Scripts/Touching.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touching : MonoBehaviour
+{
+    public LayerMask LayerMask;
+    private List<Collider> _touchingColliders = new List<Collider>();
+
+    public bool IsTouching { get { return _touchingColliders.Count > 0; } }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        _touchingColliders.Add(col);
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        _touchingColliders.Remove(col);
+    }
+}
